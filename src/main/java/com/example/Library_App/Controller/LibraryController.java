ac53273785e114452b9b4faea1017170865e6540
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-05 22:54:31 +0530

    welcome page added

--- src/main/java/com/example/Library_App/Controller/LibraryController.java
+++ src/main/java/com/example/Library_App/Controller/LibraryController.java
@@ -1,7 +1,13 @@
 package com.example.Library_App.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LibraryController {
+
+    @GetMapping("/")
+    public String Welcomepage(){
+        return "Welcome to Library App";
+    }
 }
